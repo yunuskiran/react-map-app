a6a19a0f6a86ac5edff683b5f05c651cc8b94f1e
Yunus KIRAN <yunus.kiran@turkcell.com.tr>
2022-02-20 02:48:04 +0300

    .

--- src/core/config/i18n.ts
+++ src/core/config/i18n.ts
@@ -9,9 +9,9 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     backend: {
-      loadPath: `/locales/{{lng}}/translation.json`,
+      loadPath: `${process.env.PUBLIC_URL}/locales/{{lng}}/translation.json`,
     },
-    fallbackLng: "tr",
+    fallbackLng: "en",
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
