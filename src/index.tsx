10a3ad1ee4b39aa90daa44791a6fb33e99f8e327
Yunus KIRAN <yunus.kiran@turkcell.com.tr>
2022-02-20 18:24:03 +0300

    ç

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./core/config/i18n";
 import "./mocks/server";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
